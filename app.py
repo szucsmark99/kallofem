3ae480acf10de02be08f6c7cedbd85fa1e66383b
szucsmark99 <sz.mark99@gmail.com>
2025-05-05 19:25:22 +0200

    App.py frissítése

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def run_scraper():
+def index():
     os.makedirs("public", exist_ok=True)
-    subprocess.run([
-        "scrapy", "crawl", "termekek", "-o", "public/termekek.json"
-    ])
-    return "Scraper lefutott és termekek.json létrejött."
+    subprocess.run(["scrapy", "crawl", "termekek", "-o", "public/termekek.json"])
+    return "Scraper sikeresen lefutott és elmentette a JSON-t."
 
 @app.route('/json')
 def serve_json():
     return send_file("public/termekek.json", mimetype='application/json')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port)        
