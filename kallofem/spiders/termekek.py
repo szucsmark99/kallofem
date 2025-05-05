b04ef97022d70b2c86641a01b5af68dcbeb8f252
szucsmark99 <sz.mark99@gmail.com>
2025-05-05 18:19:46 +0200

    1. commit: fájlok feltöltése

---
+++ kallofem/spiders/termekek.py
@@ -0,0 +1,17 @@
+import scrapy
+
+class TermekekSpider(scrapy.Spider):
+    name = "termekek"
+    start_urls = ["https://kallofem.hu/shop/group/keriteselemek"]
+
+    def parse(self, response):
+        for termek in response.css('div.col-6.col-md-3'):
+            yield {
+                'termeknev': termek.css('h4::text').get(default='').strip(),
+                'ar': termek.css('span.product-price::text').get(default='').strip(),
+                'kep_url': response.urljoin(termek.css('img::attr(src)').get())
+            }
+
+        kovetkezo_oldal = response.css('a.page-link[rel="next"]::attr(href)').get()
+        if kovetkezo_oldal:
+            yield response.follow(kovetkezo_oldal, callback=self.parse)
